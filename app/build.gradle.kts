180e6fb1b5e8d33755a89cf5474f206898b76c07
linyubing <480771559@qq.com>
2024-04-19 12:16:40 +0800

    高德地图显示

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(files("libs\\AMap2DMap_6.0.0_AMapSearch_9.7.1_AMapLocation_6.4.3_20240314.jar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
